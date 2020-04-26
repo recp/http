6aa9629a4dfff91e2d835704da044e7c861db10b
Recep Aslantas <info@recp.me>
2020-04-26 19:08:11 +0300

    define request parse func

---
+++ include/req.h
@@ -0,0 +1,26 @@
+/*
+* Copyright (c), Recep Aslantas.
+*
+* MIT License (MIT), http://opensource.org/licenses/MIT
+* Full license can be found in the LICENSE file
+*/
+
+#ifndef http_req_h
+#define http_req_h
+
+#include "common.h"
+#include "types.h"
+
+typedef struct http_req {
+  const char   *data;
+  const char   *url;
+  size_t        dataLen;
+  uint32_t      urlLen;
+  int           fd;
+  http_method_t method:8;
+} http_req;
+
+http_req*
+http_req_parse(const char * __restrict buff);
+
+#endif /* http_req_h */
---
+++ src/req.c
@@ -0,0 +1,14 @@
+/*
+* Copyright (c), Recep Aslantas.
+*
+* MIT License (MIT), http://opensource.org/licenses/MIT
+* Full license can be found in the LICENSE file
+*/
+
+#include "../include/req.h"
+
+http_req*
+http_req_parse(const char * __restrict buff) {
+  /* TODO */
+  return NULL;
+}
